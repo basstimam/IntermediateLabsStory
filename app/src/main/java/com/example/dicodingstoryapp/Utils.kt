5c96c76e4b100024028586d6658ff32f47848968
Imam <basstimam@gmailc.om>
2023-11-09 10:27:46 +0700

    [UPDATE] Benerin Utils

--- app/src/main/java/com/example/dicodingstoryapp/Utils.kt
+++ app/src/main/java/com/example/dicodingstoryapp/Utils.kt
@@ -1,2 +1,64 @@
 package com.example.dicodingstoryapp
 
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+
+private const val FILENAME_FORMAT = "yyyyMMdd_HHmmss"
+private val timeStamp: String = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(Date())
+
+
+fun getImageUri(context: Context){
+    var uri: Uri? = null
+
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+
+        val contentValues = ContentValues().apply {
+            put(
+                MediaStore.Images.Media.DISPLAY_NAME,
+                "IMG_${timeStamp}.jpg"
+            )
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+            put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/StoryApp")
+        }
+
+        uri = context.contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+    }
+
+    return uri ?:
+
+
+
+}
+
+
+
+
+fun getImageBeforeQLevel(context: Context): Uri {
+    val filesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    val imageFile = File(filesDir, "IMG_${timeStamp}.jpg")
+    if(
+        imageFile.parentFile?.exists() == false
+    ) imageFile.parentFile?.mkdir()
+
+    return FileProvider.getUriForFile(
+        context,
+        "${BuildConfig}.fileprovider",
+        imageFile
+    )
+
+}
+
--- app/src/main/java/com/example/dicodingstoryapp/activity/PostStoryActivity.kt
+++ app/src/main/java/com/example/dicodingstoryapp/activity/PostStoryActivity.kt
@@ -1,15 +1,59 @@
 package com.example.dicodingstoryapp.activity
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.dicodingstoryapp.databinding.ActivityPostStoryBinding
 
 class PostStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostStoryBinding
+    private var currentImageUri: Uri? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityPostStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            galeryFab.setOnClickListener {
+                startGallery()
+            }
+        }
+    }
+
+
+
+
+
+
+    private fun startGallery(){
+        launcherGallery.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
+    }
+
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage(){
+        currentImageUri?.let{
+            Log.d("Photo Picker", "Uri: $it")
+            binding.previewImageView.setImageURI(it)
+        }
     }
 }
